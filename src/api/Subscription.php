ba40a13c9f119148d83a1f1d229e2f8d5d6d3fdf
Leo Sjöberg <leo.sjoberg@gmail.com>
2015-08-13 18:54:35 +0200

    plans can't be updated, subscriptions can...

--- src/api/Subscription.php
+++ src/api/Subscription.php
@@ -13,4 +13,11 @@ class Subscription extends ApiBase
 
         return HttpHelper::get($this->username, $this->password, $remote_url);
     }
+
+    public function update($id, array $params)
+    {
+        $remote_url = $this->endpoint('subscriptions/'.$id);
+
+        return HttpHelper::put($this->username, $this->password, $remote_url, $params);
+    }
 }
