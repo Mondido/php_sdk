39cb34d728d5f9ea7b22d112e8b62202bb78be13
Leo Sjöberg <leo.sjoberg@gmail.com>
2015-08-24 13:43:19 +0200

    Added docblocks and a method to create subscriptions

--- src/Api/Subscription.php
+++ src/Api/Subscription.php
@@ -7,18 +7,61 @@ use Mondido\HttpHelper;
 
 class Subscription extends ApiBase
 {
+    /**
+     * Retrieve all subscriptions.
+     * 
+     * @param  array  $options
+     * @return array
+     */
     public function all($options = array())
     {
         return $this->request('get', 'subscriptions', $options);
     }
 
+    /**
+     * Update a given subscription.
+     * 
+     * @param  int $id
+     * @param  array  $params
+     * @return array
+     */
     public function update($id, array $params)
     {
         return $this->request('put', 'subscriptions/'.$id, $params);
     }
 
+    /**
+     * Fetch a given subscription.
+     * 
+     * @param  int $id
+     * @return array
+     */
     public function get($id)
     {
         return $this->request('get', 'subscriptions/'.$id);
+    }
+
+    /**
+     * Create a new subscription.
+     * @param  string $planId
+     * @param  int|string $identifier
+     * @param  string $identifierType
+     * @return array
+     */
+    public function create($planId, $identifier, $identifierType = 'ref')
+    {
+        $types = [
+            'card' => 'stored_card_id',
+            'ref' => 'customer_ref',
+            'customer' => 'customer_id'
+        ];
+        
+        $type = isset($types[$identifierType]) ? $types[$identifierType] : $identifierType;
+
+        return $this->request('post', 'subscriptions', [
+            'plan_id'   => $planId,
+            $type       => $identifier,
+            'quantity'  => 1,
+        ]);
     }
 }
