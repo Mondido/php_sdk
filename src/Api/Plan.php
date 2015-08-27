573b2479b5d5e466e857939deb994d35c8533d5d
Leo Sjöberg <leo.sjoberg@gmail.com>
2015-08-27 11:43:30 +0200

    Allow users to pass the filter directly without wrapping it as an array. E.g using 'name' => 'test', rather than 'filter[name]' => 'test'

--- src/Api/Plan.php
+++ src/Api/Plan.php
@@ -9,8 +9,21 @@ class Plan extends ApiBase
 {
     public function all($options = array())
     {
+    	foreach ($options as $filter => $value) {
+    		if (substr($filter, 0, 6) != 'filter') {
+    			// The user probably wants to filter the request
+    			// but didn't format it correctly, so we do it.
+    			$options["filter[{$filter}]"] = $value;
+                unset ($options[$filter]);
+    		}
+    	}
         $remote_url = $this->endpoint('plans') . '?' . http_build_query($options);
 
         return HttpHelper::get($this->username, $this->password, $remote_url);
+    }
+
+    public function get($id)
+    {
+    	return $this->request('get', 'plans/'.$id);
     }
 }
